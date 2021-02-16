7961d078d92fa7745b4c7994ed269e123bf3b0f8
feichuanshuo <1643203787@qq.com>
2021-02-16 23:50:50 +0800

    1.1

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter,Route,Switch,Redirect} from 'react-router-dom'
 import './index.css';
-import Home from "./Home/plus";
-import AuthorHome from "./AuthorHome/plus";
+import Home from "./Home";
+import AuthorHome from "./AuthorHome";
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
     <BrowserRouter>
